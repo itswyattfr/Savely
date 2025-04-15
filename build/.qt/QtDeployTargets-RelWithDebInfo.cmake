c4f68319deb87c56976fec1cb377ac059071c61c
itswyattfr <156371236+itswyattfr@users.noreply.github.com>
2025-04-14 22:11:50 -0400

    Remove ./build | Add .gitignore

--- build/.qt/QtDeployTargets-RelWithDebInfo.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Savely_FILE C:/Users/wyatt/Desktop/Savely/build/RelWithDebInfo/Savely.exe)
-set(__QT_DEPLOY_TARGET_Savely_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Savely_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Network.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Core.dll)
--- build/Qt_6_9_0_msvc2022_64_temporary/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Savely_FILE C:/Users/wyatt/Desktop/Savely/build/Qt_6_9_0_msvc2022_64_temporary/Release/Savely.exe)
-set(__QT_DEPLOY_TARGET_Savely_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Savely_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Core.dll)
